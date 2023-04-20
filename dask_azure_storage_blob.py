5eda6c1db1ea99714a6879ecdbd5295e3e559a21
Tom Augspurger <taugspurger@microsoft.com>
2023-04-20 11:19:10 -0500

    fixup

--- dask_azure_storage_blob.py
+++ dask_azure_storage_blob.py
@@ -1,16 +1,31 @@
 """
 dask-azure-blob-storage
 """
+import datetime
 import random
 import dask
+import dask.bag
 from typing import Optional, Union, Dict, TypedDict
 
 import azure.storage.blob
-from azure.core.credentials import AzureNamedKeyCredential, AzureSasCredential, TokenCredential
+from azure.core.credentials import (
+    AzureNamedKeyCredential,
+    AzureSasCredential,
+    TokenCredential,
+)
+import pandas as pd
 
 __version__ = "0.1.0"
 
-CredentialT = Optional[Union[str, Dict[str, str], "AzureNamedKeyCredential", "AzureSasCredential", "TokenCredential"]]  # pylint: disable=line-too-long
+CredentialT = Optional[
+    Union[
+        str,
+        Dict[str, str],
+        "AzureNamedKeyCredential",
+        "AzureSasCredential",
+        "TokenCredential",
+    ]
+]
 
 
 class StorageOptions(TypedDict):
@@ -20,12 +35,16 @@ class StorageOptions(TypedDict):
 
 
 @dask.delayed
-def list_prefixes(prefix: str, depth: int, storage_options: StorageOptions) -> list[str]:
+def list_prefixes(
+    prefix: str, depth: int, storage_options: StorageOptions
+) -> list[str]:
     """
     List the prefixes at a given depth under a prefix.
 
     This can be useful for listing blobs (or "folders") quickly. This will
     call itself recursively, using Dask's support for nested parallelism.
+
+    Combine with get_blob_properties to ...
 
     Parameters
     ----------
@@ -49,15 +68,12 @@ def list_prefixes(prefix: str, depth: int, storage_options: StorageOptions) -> l
     blob_names = []
     with cc:
         if d < depth:
-            print("walking", prefix)
             prefixes = [x.name for x in cc.walk_blobs(prefix)]
             if d == depth - 1:
                 # no need to recurse here, just to return it.
                 blob_names.extend(prefixes)
             else:
-                xs = [
-                    list_prefixes(x, depth, storage_options) for x in prefixes
-                ]
+                xs = [list_prefixes(x, depth, storage_options) for x in prefixes]
                 xs = dask.compute(*xs)
                 for x in xs:
                     blob_names.extend(x)
@@ -77,10 +93,25 @@ def read_under_prefix(x: str, storage_options, sample=1):
     items = []
     blobs = list(cc.list_blobs(x))
     blobs = list(random.sample(blobs, int(len(blobs) * sample)))
-    
+
     with cc:
         for blob in blobs:
             content = cc.get_blob_client(blob).download_blob().readall()
             items.append(content)
-        
+
     return items
+
+
+def list_properties(
+    prefix: str, storage_options: StorageOptions
+) -> list[tuple[str, datetime.datetime, datetime.datetime, int]]:
+    """
+    List the properties of all blobs under a prefix.
+    """
+    with azure.storage.blob.ContainerClient(**storage_options) as cc:
+        blobs_properties = cc.list_blobs(prefix)
+        records = [
+            (bp.name, bp.last_modified, bp.creation_time, bp.size)
+            for bp in blobs_properties
+        ]
+    return records
